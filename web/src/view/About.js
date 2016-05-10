881e9a9401ab4f99f6351892f18e4b4147f3f267
lucasjsoliveira <lucasjsoliveira@gmail.com>
2016-05-10 09:07:25 -0300

    Mexido conteudo de about

--- web/src/view/About.js
+++ web/src/view/About.js
@@ -3,10 +3,25 @@
  */
 import React from 'react';
 
-var About = React.createClass( {
+var About = React.createClass({
     render () {
         return (
-            <div>Página about!</div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam tortor vel dictum consequat. Ut
+                    iaculis et purus vel cursus. In hac habitasse platea dictumst. Donec imperdiet odio sed viverra
+                    facilisis. Pellentesque ut rhoncus leo, et euismod orci. Integer at semper eros, non tincidunt
+                    velit. Duis varius nulla ex, vitae pretium ex hendrerit eu. Nunc nec nisi ac orci feugiat gravida ut
+                    a lorem. Quisque gravida fringilla efficitur. Cum sociis natoque penatibus et magnis dis parturient
+                    montes, nascetur ridiculus mus. Donec pharetra tristique sodales. Integer feugiat libero quam, non
+                    consequat sem pharetra id. Aenean mauris sapien, placerat id sollicitudin eu, condimentum eget
+                    erat.</p>
+
+                <p>Vivamus commodo consequat lorem vehicula blandit. Aenean a varius neque. Etiam ullamcorper enim quis
+                    auctor porttitor. Donec eu varius sapien. Ut euismod, purus nec volutpat condimentum, risus nulla
+                    rhoncus dui, at sodales nisl nunc in ligula. Suspendisse potenti. Morbi nec porta mauris. Proin in
+                    bibendum felis. Maecenas molestie nisi ut orci condimentum feugiat. Morbi eu semper metus, ut mattis
+                    sem. Nunc id egestas libero. Maecenas id nisl laoreet, consectetur est quis, lobortis augue.</p>
+            </div>
         )
     }
 });
