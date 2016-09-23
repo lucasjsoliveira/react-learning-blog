48e1cf67ee84fd90ecc1395d0c9f26f1dfea2e07
lucasjsoliveira <lucasjsoliveira@gmail.com>
2016-09-23 11:03:11 -0300

    Adicionado depurador de mobX

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import {Notifications} from './components/notification';
+import DevTools from 'mobx-react-devtools';
+
 export default class App extends React.Component {
     render() {
+        var devToolsPosition = {right: 5, bottom: 0};
         return (
             <div>
+                <DevTools position={devToolsPosition}/>
                 <Navbar />
                 <Notifications />
                 <div className="container">{this.props.children}</div>
