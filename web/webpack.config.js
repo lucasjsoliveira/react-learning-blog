d0f7e757da0c03fd16feb54f93fd4be1c83fe6d0
lucasjsoliveira <lucasjsoliveira@gmail.com>
2016-12-23 13:36:51 -0200

    Atualização de método de bundling de dependências webpack

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,13 +1,19 @@
 var webpack = require('webpack');
-var SplitByPathPlugin = require('webpack-split-by-path');
 
 module.exports = {
     // Aqui você define o arquivo de "entrada" do webpack. ele vai, a partir daqui, colocar todos os imports
     // no arquivo de saída.
-    entry: {app: './src/index.js'},
+    entry: {
+        app: './src/index.js',
+        vendor: [
+            "es6-promise","exports-loader","imports-loader",
+            "mobx","mobx-react","moment","react","react-dom",
+            "react-paginate-component","react-router","react-tether",
+            "whatwg-fetch"
+        ]
+    },
     output: {
-        filename: './js/[name].js',
-        chunkFilename: './js/[name].js'
+        filename: './js/bundle.js'
     },
     module: {
         loaders: [
@@ -19,12 +25,6 @@ module.exports = {
         new webpack.ProvidePlugin({
             'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
         }),
-
-        // Isso aqui é um plugin que me permite separar meu código em um js de saída diferente da pasta node_modules.
-        // o output do meu webpack são 3 arquivos, vendor.js, app.js (nome este definido lá em cima no entry) e um
-        // manifest.js (que o plugin SplitByPath gera por algum motivo pra manter todos os arquivos separados funcionando.
-        new SplitByPathPlugin([
-                {name: 'vendor', path: __dirname + '/node_modules'}
-            ], [])
+        new webpack.optimize.CommonsChunkPlugin("vendor", "./js/vendor.bundle.js")
     ]
 };
